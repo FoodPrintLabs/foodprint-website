fa266942aa82cdd3af84e0605ccc09c9db84a49a
nkululekombhele <nkululekombhele6017@gmail.com>
2023-04-16 22:59:00 +0200

    Create contact form model

---
+++ models/foodprint_contact.js
@@ -0,0 +1,47 @@
+const Sequelize = require('sequelize');
+module.exports = function (sequelize, DataTypes) {
+  return sequelize.define(
+    'FoodprintContact',
+    {
+      pk: {
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+      },
+      firstname: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+      },
+      surname: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+      },
+      email: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+      },
+      message: {
+        type: DataTypes.STRING(765),
+        allowNull: true,
+      },
+      logdatetime: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+    },
+    {
+      sequelize,
+      tableName: 'foodprint_subscription',
+      timestamps: false,
+      indexes: [
+        {
+          name: 'foodprint_subscription_PRIMARY',
+          unique: true,
+          using: 'BTREE',
+          fields: [{ name: 'pk' }],
+        },
+      ],
+    }
+  );
+};
